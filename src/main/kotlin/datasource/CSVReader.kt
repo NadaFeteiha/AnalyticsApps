ff908b60bb4395c9553067349240bd722c5ab7a3
Nada Feteiha <nada.feteiha@gmail.com>
2022-06-03 09:18:24 -0400

    Revert "Edit:make the conditions have body like review notation,replace all it in function,remove the variable instance."
    This reverts commit 1a43048044d446880da89bc547a82ca7950ca52a.

--- src/main/kotlin/datasource/CSVReader.kt
+++ src/main/kotlin/datasource/CSVReader.kt
@@ -4,16 +4,20 @@ import utilities.Constant
 import java.io.File
 
 class CSVReader {
+
     fun getTableRows(fileName: String): List<String>? {
         val tableRows = mutableListOf<String>()
-            if (!fileName.contains(Constant.SUFFIX_FILE_NAME)) {
-                fileName.plus(Constant.SUFFIX_FILE_NAME) }
-            else { fileName }.also {fName->
-                File(fName).apply {
-                    if (this.exists()) { this.forEachLine { row -> tableRows.add(row) } }
-                    else { return null }
+        val fileNameWithSuffix =
+            if (!fileName.contains(Constant.SUFFIX_FILE_NAME)) fileName.plus(Constant.SUFFIX_FILE_NAME) else fileName
+
+        File(fileNameWithSuffix).apply {
+            if (this.exists()) {
+                this.forEachLine { row ->
+                    tableRows.add(row)
                 }
-            }
+            } else
+                return null
+        }
         return tableRows
     }
 }
--- src/main/kotlin/utilities/Converter.kt
+++ src/main/kotlin/utilities/Converter.kt
@@ -6,15 +6,16 @@ import kotlin.math.pow
 
 
 fun Int.calculatePercentage(divisor: Int): Double? {
-    return if (divisor != 0) {
-        String.format("%.1f", 100.0 * this.div(divisor.toDouble())).toDouble() }
-    else { null }
+    return  if (divisor != 0) {
+        String.format("%.1f", 100.0 * this.div(divisor.toDouble())).toDouble()
+    } else {
+        null
+    }
 }
 
-fun String.convertToDouble(): Double? {
-    return this.split(" ").first().toDoubleOrNull() }
+fun String.convertToDouble(): Double? = this.split(" ").first().toDoubleOrNull()
 
-fun String.stringToDate(): Date {
+fun String.stringToDate(): Date? {
     return SimpleDateFormat(Constant.DATE_FORMAT).parse(this)
 }
 
