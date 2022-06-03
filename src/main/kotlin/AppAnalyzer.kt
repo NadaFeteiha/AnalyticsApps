1a43048044d446880da89bc547a82ca7950ca52a
muslim <n00rALDENakel>
2022-06-03 06:14:10 +0300

    Edit:make the conditions have body like review notation,replace all it in function,remove the variable instance.

--- src/main/kotlin/AppAnalyzer.kt
+++ src/main/kotlin/AppAnalyzer.kt
@@ -4,64 +4,47 @@ import utilities.calculatePercentage
 
 class AppAnalyzer {
 
-    //Refactor
     fun findAppDevelopedByGivenCompany(apps: List<App>, companyName: String): Int? {
         return if (apps.isNotEmpty() && companyName.isNotEmpty())
             apps.count { app: App -> app.company.contains(companyName.trim(), true) }
-        else {
-            null
-        }
+        else {null}
+    }
+    fun findPercentageOfAppsByCategory(apps: List<App>, categoryName: String): Double? {
+        return if (apps.isNotEmpty() && categoryName.isNotEmpty()) {
+            apps.count { count-> count.category.contains(categoryName.trim(), true) }.calculatePercentage(apps.size)}
+        else {null}
     }
 
-    //Refactor
-    fun findPercentageOfAppsByCategory(apps: List<App>, categoryName: String): Double? =
-        if (apps.isNotEmpty() && categoryName.isNotEmpty()) {
-            apps.count { it.category.contains(categoryName.trim(), true) }
-                .calculatePercentage(apps.size)
-        } else {
-            null
-        }
+    fun findOldestApp(apps: List<App>): App? {
+        return if (apps.isNotEmpty()) { apps.minByOrNull { selector -> selector.updated}}
+        /* don't use !! because u certainly the value is existing but if some cause is not the app  will crash */
+        else {null}
+    }
 
-    //Refactor
-    fun findOldestApp(apps: List<App>): App? =
-        if (apps.isNotEmpty()) {
-            apps.filterNot { app -> app.updated == null }.minByOrNull { selector -> selector.updated!! }
-        } else {
-            null
-        }
-
-    //Refactor
     fun findPercentageOfAppRunningOnSpecificAndroid(apps: List<App>, version: Double): Double? {
         return if (apps.isNotEmpty() && version >= Constant.MIN_COMPARE_INT ) {
             apps.count { count -> count.requiresAndroid != null && count.requiresAndroid == version }
-                .calculatePercentage(apps.size)
-        } else {
-            null
-        }
+                .calculatePercentage(apps.size) }
+        else {null}
     }
 
-    //Refactor
     fun findLargestApps(app: List<App>, rankSize: Int): List<App>? {
         return if (app.isNotEmpty() && rankSize in Constant.MIN_COMPARE_INT.. app.size  ) {
-            app.sortedByDescending { it.size }.toList().take(rankSize)
-        }else {null}
+            app.sortedByDescending {dataSorted -> dataSorted.size }.toList().take(rankSize) }
+        else {null}
     }
-
-    //Replace
     fun topAppInstall(apps: List<App>, size: Int): List<App>? {
         return if (apps.isNotEmpty() &&  size in Constant.MIN_COMPARE_INT.. apps.size) {
             apps.sortedByDescending { dataSorted -> dataSorted.installs }
                 .take(size)
-                .toList()
-        } else {
-            null
-        }
+                .toList()}
+        else {null}
     }
 
     fun getLargestAppSizeByCompanyName(apps: List<App>,companyName:String): App? {
         return if (apps.isNotEmpty() && companyName.isNotEmpty()) {
-           val largestApp = apps.filter { it.company.contains(companyName.trim(), true) }
-            findLargestApps(largestApp, 1)?.first()
-        } else null
+            apps.filter { app: App ->  app.company.contains(companyName.trim(), true) }.also { list->
+                findLargestApps(app = list, rankSize = 1) }.first() }
+        else {null}
     }
 }
--- src/main/kotlin/datasource/CSVReader.kt
+++ src/main/kotlin/datasource/CSVReader.kt
@@ -4,20 +4,16 @@ import utilities.Constant
 import java.io.File
 
 class CSVReader {
-
     fun getTableRows(fileName: String): List<String>? {
         val tableRows = mutableListOf<String>()
-        val fileNameWithSuffix =
-            if (!fileName.contains(Constant.SUFFIX_FILE_NAME)) fileName.plus(Constant.SUFFIX_FILE_NAME) else fileName
-
-        File(fileNameWithSuffix).apply {
-            if (this.exists()) {
-                this.forEachLine { row ->
-                    tableRows.add(row)
+            if (!fileName.contains(Constant.SUFFIX_FILE_NAME)) {
+                fileName.plus(Constant.SUFFIX_FILE_NAME) }
+            else { fileName }.also {fName->
+                File(fName).apply {
+                    if (this.exists()) { this.forEachLine { row -> tableRows.add(row) } }
+                    else { return null }
                 }
-            } else
-                return null
-        }
+            }
         return tableRows
     }
 }
