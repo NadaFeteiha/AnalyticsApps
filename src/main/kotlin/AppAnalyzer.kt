67b0850d30ee1b90f80b6176aa11abd95714cab0
Nooralden <nooraldenakel@gmail.com>
2022-06-10 08:15:19 +0300

    edit:enhance analyze fun by give covenant name instead of it in lambda.

--- src/main/kotlin/AppAnalyzer.kt
+++ src/main/kotlin/AppAnalyzer.kt
@@ -5,36 +5,33 @@ import utilities.calculatePercentage
 class AppAnalyzer {
 
     fun findAppDevelopedByGivenCompany(apps: List<App>, companyName: String): Int? {
-        return getAllAppsCompany(apps, companyName)?.size //using count instead of size
+        return getAllAppsCompany(apps, companyName)?.count()
     }
 
     fun findPercentageOfAppsByCategory(apps: List<App>, categoryName: String): Double? {
         return if (apps.isNotEmpty() && categoryName.isNotEmpty()) {
-            apps.count { it.category.contains(categoryName.trim(), true) } //replace it with the convenient name
+            apps.count { count -> count.category.contains(categoryName.trim(), true) }
                 .calculatePercentage(apps.size)
-        } else {
-            null
         }
+        else { null }
     }
 
     fun findOldestApp(apps: List<App>): App? {
         return if (apps.isNotEmpty()) {
             apps.filterNot { app -> app.updated == null }.minByOrNull { selector -> selector.updated!! }
-        } else {
-            null
         }
+        else { null }
     }
 
     fun findPercentageOfAppRunningOnSpecificAndroid(apps: List<App>, version: Double): Double? {
         return if (apps.isNotEmpty() && version >= Constant.MIN_COMPARE_INT) {
             apps.count { count -> count.requiresAndroid != null && count.requiresAndroid == version }
                 .calculatePercentage(apps.size)
-        } else {
-            null
         }
+        else { null }
     }
 
-    fun findLargestApps(app: List<App>, rankSize: Int): List<App>? {
+    fun findLargestApps(app: List<App>, rankSize: Int ): List<App>? {
         return if (app.isNotEmpty() && rankSize in Constant.MIN_COMPARE_INT..app.size) {
             app.sortedByDescending { dataSorted -> dataSorted.size }.take(rankSize)
         }
@@ -50,16 +47,15 @@ class AppAnalyzer {
     }
 
     fun getLargestAppSizeByCompanyName(apps: List<App>, companyName: String): App? {
-        return getAllAppsCompany(apps, companyName)?.let {
-            findLargestApps(it, 1)?.first() //replace it with the convenient name
+        return getAllAppsCompany(apps, companyName)?.let {appList ->
+            findLargestApps(appList,1)?.first() //replace magic number or make it optional
         }
     }
 
     private fun getAllAppsCompany(apps: List<App>, companyName: String): List<App>? {
         return if (apps.isNotEmpty() && companyName.isNotEmpty()) {
-            apps.filter { it.company.contains(companyName.trim(), true) } //replace it with the convenient name
-        } else {
-            null
+            apps.filter { app: App ->  app.company.contains(companyName.trim(), true) }
         }
+        else { null }
     }
 }
