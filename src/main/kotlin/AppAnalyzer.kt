e3fdb85941711b470d11f8f09b0218025b72c5a2
Nooralden <nooraldenakel@gmail.com>
2022-06-10 08:02:57 +0300

    edit:enhance analyze fun by remove non-useful build-in functions.

--- src/main/kotlin/AppAnalyzer.kt
+++ src/main/kotlin/AppAnalyzer.kt
@@ -5,12 +5,12 @@ import utilities.calculatePercentage
 class AppAnalyzer {
 
     fun findAppDevelopedByGivenCompany(apps: List<App>, companyName: String): Int? {
-        return getAllAppsCompany(apps, companyName)?.size
+        return getAllAppsCompany(apps, companyName)?.size //using count instead of size
     }
 
     fun findPercentageOfAppsByCategory(apps: List<App>, categoryName: String): Double? {
         return if (apps.isNotEmpty() && categoryName.isNotEmpty()) {
-            apps.count { it.category.contains(categoryName.trim(), true) }
+            apps.count { it.category.contains(categoryName.trim(), true) } //replace it with the convenient name
                 .calculatePercentage(apps.size)
         } else {
             null
@@ -36,31 +36,28 @@ class AppAnalyzer {
 
     fun findLargestApps(app: List<App>, rankSize: Int): List<App>? {
         return if (app.isNotEmpty() && rankSize in Constant.MIN_COMPARE_INT..app.size) {
-            app.sortedByDescending { it.size }.toList().take(rankSize)
-        } else {
-            null
+            app.sortedByDescending { dataSorted -> dataSorted.size }.take(rankSize)
         }
+        else { null }
     }
 
     fun topAppInstall(apps: List<App>, size: Int): List<App>? {
         return if (apps.isNotEmpty() && size in Constant.MIN_COMPARE_INT..apps.size) {
             apps.sortedByDescending { dataSorted -> dataSorted.installs }
                 .take(size)
-                .toList()
-        } else {
-            null
         }
+        else { null }
     }
 
     fun getLargestAppSizeByCompanyName(apps: List<App>, companyName: String): App? {
         return getAllAppsCompany(apps, companyName)?.let {
-            findLargestApps(it, 1)?.first()
+            findLargestApps(it, 1)?.first() //replace it with the convenient name
         }
     }
 
     private fun getAllAppsCompany(apps: List<App>, companyName: String): List<App>? {
         return if (apps.isNotEmpty() && companyName.isNotEmpty()) {
-            apps.filter { it.company.contains(companyName.trim(), true) }
+            apps.filter { it.company.contains(companyName.trim(), true) } //replace it with the convenient name
         } else {
             null
         }
